86462e5dcf1bcf046770b19e7e113e89e6950226
Michelle Athayde <michelle.estevinho@hotmail.com>
2021-01-16 02:31:11 -0300

    Formularios e templates

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     templateUrl: './course-info.component.html'
@@ -7,13 +9,18 @@ import { ActivatedRoute } from "@angular/router";
 
 export class CourseInfoComponent implements OnInit{
     
-    courseId: number;
+    course: Course;
 
     constructor(
-        private activatedRoute: ActivatedRoute
+        private activatedRoute: ActivatedRoute,
+        private courseService: CourseService
     ) { }
     
     ngOnInit() { 
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
     }
 }
